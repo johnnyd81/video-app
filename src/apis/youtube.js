f78620700ab3ed1c4b83be1611613d48f27ed0c3
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-09-12 17:06:20 +0200

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,6 +3,7 @@ import axios from "axios";
 //the api key has been stored in a .env file and is referenced using process.env
 const KEY = process.env.REACT_APP_API_KEY;
 
+//axios is used to get the data for the videos
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
